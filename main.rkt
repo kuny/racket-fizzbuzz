b93784c0a03e81c2eff01fbac1f29ba5e82c5acc
Kunihisa Matsuda <love_coke@mac.com>
2026-04-08 16:04:54 +0900

    fix

--- main.rkt
+++ main.rkt
@@ -9,7 +9,7 @@
   (stream-map (lambda (x)
                 (cond ((modulo-zero? x 15) 'FizzBuzz)
                       ((modulo-zero? x 3) 'Fizz)
-                      ((modulo-zero? x 15) 'Buzz)
+                      ((modulo-zero? x 5) 'Buzz)
                       (else x)))
               (in-range 1 101)))
 
