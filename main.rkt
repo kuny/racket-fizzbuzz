3b0688c89f6ca27678baef36925379c92006fb0e
Kunihisa Matsuda <love_coke@mac.com>
2026-04-08 15:16:10 +0900

    fix code

--- main.rkt
+++ main.rkt
@@ -1,10 +1,15 @@
 #lang racket
 
+(define-syntax modulo-zero?
+  (syntax-rules ()
+    [(modulo-zero? x n)
+     (zero? (modulo x n))]))
+
 (stream->list
   (stream-map (lambda (x)
-                (cond ((zero? (modulo x 15)) 'FizzBuzz)
-                      ((zero? (modulo x 3)) 'Fizz)
-                      ((zero? (modulo x 15)) 'Buzz)
+                (cond ((modulo-zero? x 15) 'FizzBuzz)
+                      ((modulo-zero? x 3) 'Fizz)
+                      ((modulo-zero? x 15) 'Buzz)
                       (else x)))
               (in-range 1 101)))
 
